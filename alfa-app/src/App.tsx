f701aaf4c59633beb3dad52fe65d610bc08c1fd3
Pavel Gordienko <guzmo@yandex.ru>
2024-09-05 01:12:34 +0300

    feat: get breed data

--- alfa-app/src/App.tsx
+++ alfa-app/src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+interface BreedList {
+  [key: string]: string[];
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const url = "https://dog.ceo/api/breeds/list/all";
+  const [breeds, SetBreeds] = useState<BreedList | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        SetBreeds(response.data.message);
+        setIsLoading(false);
+      })
+      .catch((error: Error) => {
+        console.error("Error fetching data:", error);
+        setError("Error loading breeds");
+        setIsLoading(false);
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Products</h1>
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <ul>
+          {breeds &&
+            Object.keys(breeds).map((breedName) => (
+              <li key={breedName}>{breedName}</li>
+            ))}
+        </ul>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
